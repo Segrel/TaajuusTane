51a6968942935a4766a7a2397acc4fdab1974814
Segrel Koskentausta <segrealm@gmail.com>
2020-11-05 23:19:27 +0200

    Lisää luokka tallenteen luvulle tiedostosta

---
+++ app/src/main/java/TaajuusTane/Tallenne.java
@@ -0,0 +1,52 @@
+package TaajuusTane;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+/** Luokka Tallenne-tiedostojen käsittelylle. */
+public class Tallenne {
+  /** Lue Tallenne-tiedosto.
+  *
+  * @param tiedosto Viite luettavaan tiedostoon
+  * @return Taulukko ääninäytteitä
+  * @throws Exception Tiedoston käsittelyssä tapahtui virhe
+  * @throws IOException Tiedoston luvussa tapahtui virhe
+  * @throws UnsupportedAudioFileException Tallenteen audioformaatti ei ole tuettu
+  */
+  public static Kompleksi[] lue(File tiedosto) throws Exception, IOException, UnsupportedAudioFileException {
+    AudioInputStream audiovuo = AudioSystem.getAudioInputStream(tiedosto);
+    AudioFormat metadata = audiovuo.getFormat();
+    if (!metadata.getEncoding().equals(AudioFormat.Encoding.PCM_SIGNED)) {
+      throw new Exception("Tallenteen tulee olla etumerkillisessä PCM-muodossa.");
+    }
+    if (metadata.isBigEndian()) {
+      throw new Exception("Tallenteen tulee olla little-endian tavujärjestyksessä.");
+    }
+    if (metadata.getFrameRate() != 44100) {
+      throw new Exception("Tallenteen näytteenottotaajuuden tulee olla 44.1 kHz.");
+    }
+    if (metadata.getFrameSize() != 2) {
+      throw new Exception("Tallenteen näytteiden bittimäärän tulee olla 16.");
+    }
+    if (metadata.getChannels() != 1) {
+      throw new Exception("Tallenteen tulee olla yksikanavainen.");
+    }
+    int pituus = (int) audiovuo.getFrameLength();
+    Kompleksi[] data = new Kompleksi[pituus];
+    ByteBuffer puskuri = ByteBuffer.allocate(2);
+    puskuri.order(ByteOrder.LITTLE_ENDIAN);
+    for (int i = 0; i < pituus; i += 1) {
+      if (audiovuo.read(puskuri.array()) == -1) {
+        throw new Exception(String.format("Oletettiin %d näytettä, luettiin vain %d.", pituus, (i + 1)));
+      }
+      data[i] = new Kompleksi(puskuri.getShort(0), 0.0);
+    }
+    return data;
+  }
+}
---
+++ app/src/test/java/TaajuusTane/TallenneTesti.java
@@ -0,0 +1,9 @@
+package TaajuusTane;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TallenneTesti {
+  // todo
+}
