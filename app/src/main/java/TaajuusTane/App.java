8323fd1157377fbb8c361301ff7a3fd2ea14cb44
Segrel Koskentausta <segrealm@gmail.com>
2020-11-13 16:32:10 +0200

    Luonnostelma kokonaistoiminnallisuudesta

--- app/src/main/java/TaajuusTane/App.java
+++ app/src/main/java/TaajuusTane/App.java
@@ -13,10 +13,20 @@ public class App {
     if (args.length == 0) {
       throw new Exception("Anna komentoriviargumenttina polku tallenteeseen.");
     }
-    Kompleksi[] signaali;
-    signaali = Tallenne.lue(new File(args[0]));
-    System.out.println("Luettiin audiotiedostosta näytteitä (kpl): " + Integer.toString(signaali.length));
-    // todo tee fourier-muunnos
-    // todo analysoi muunnoksen lopputulos
+    Kompleksi[] signaali = Tallenne.lue(new File(args[0]));
+    System.out.println("Luettiin audiotiedostosta " + Integer.toString(signaali.length) + " näytettä.");
+    Kompleksi[] muunnos = Fourier.muunnos(signaali);
+    double suurin = 0.0;
+    int suurinIndeksi = 0;
+    // Meitä kiinnostaa vain ensimmäiset ~22kHz
+    for (int i = 0; i < (muunnos.length / 2); i += 1) {
+      double m = Kompleksi.moduli(muunnos[i]);
+      if (m > suurin) {
+        suurin = m;
+        suurinIndeksi = i;
+      }
+    }
+    int perustaajuus = (int) ((44100.0 / muunnos.length) * suurinIndeksi);
+    System.out.println("Analyysi: perustaajuus on " + Integer.toString(perustaajuus) + "Hz.");
   }
 }
--- app/src/test/java/TaajuusTane/AppTesti.java
+++ app/src/test/java/TaajuusTane/AppTesti.java
@@ -11,15 +11,29 @@ import org.mockito.MockedStatic;
 
 public class AppTesti {
   @Test public void mainToimii() throws Exception {
-    Kompleksi[] data = new Kompleksi[5];
-    MockedStatic<Tallenne> t = mockStatic(Tallenne.class);
-    t.when(() -> Tallenne.lue(any(File.class))).thenReturn(data);
+    Kompleksi[] tallenneData = {
+      new Kompleksi(-4.0, 0.0),
+      new Kompleksi(-1.0, 1.0),
+      new Kompleksi(3.0, 0.0),
+      new Kompleksi(2.0, 1.0),
+      };
+    Kompleksi[] muunnosData = {
+      new Kompleksi(0.0, 2.0),
+      new Kompleksi(-7.0, 3.0),
+      new Kompleksi(-2.0, -2.0),
+      new Kompleksi(-7.0, -3.0),
+      };
     String[] args = new String[1];
     args[0] = "/tiedostopolku";
+    MockedStatic<Tallenne> t = mockStatic(Tallenne.class);
+    t.when(() -> Tallenne.lue(any(File.class))).thenReturn(tallenneData);
+    MockedStatic<Fourier> f = mockStatic(Fourier.class);
+    t.when(() -> Fourier.muunnos(tallenneData)).thenReturn(muunnosData);
 
     App app = new App();
     app.main(args);
     t.verify(() -> Tallenne.lue(any(File.class)));
+    t.verify(() -> Fourier.muunnos(tallenneData));
   }
 
   @Test(expected = Exception.class)
