9e137c55f41c3d758a90d5c3eadd401401b6467f
Segrel Koskentausta <segrealm@gmail.com>
2020-11-17 00:09:33 +0200

    Lisää lyhyt 440Hz siniaallon sisältävä WAV-tiedosto

--- skriptit/genwav.py
+++ skriptit/genwav.py
@@ -1,7 +1,7 @@
 import wave, struct, math, random
 
 naytteenOttoTaajuus = 44100.0 # hertsiä
-kesto = 5.0 # sekuntia
+kesto = 0.75 # sekuntia
 taajuus = 440.0 # HZ
 
 clip = wave.open('440hz.wav', 'w')
@@ -12,8 +12,6 @@ clip.setframerate(naytteenOttoTaajuus)
 
 for i in range(int(naytteenOttoTaajuus * kesto)):
    arvo = math.sin(2 * math.pi * taajuus * i / naytteenOttoTaajuus)
-   if i < 25:
-     print(int(arvo * 32767.0))
    data = struct.pack('<h', int(arvo * 32767.0))
    clip.writeframes(data)
 
